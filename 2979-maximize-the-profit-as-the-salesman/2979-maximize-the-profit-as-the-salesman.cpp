1b6de256294963f1a2dafeb40a40bd517021451d
Dhruv Garg <77916132+dhruvg24@users.noreply.github.com>
2023-08-20 12:38:30 +0530

    Time: 515 ms (100%), Space: 151.7 MB (50%) - LeetHub

--- 2979-maximize-the-profit-as-the-salesman/2979-maximize-the-profit-as-the-salesman.cpp
+++ 2979-maximize-the-profit-as-the-salesman/2979-maximize-the-profit-as-the-salesman.cpp
@@ -1,48 +1,21 @@
 class Solution {
-    public:
-    int findIdx(int idx, int lastIdx, vector<vector<int>> &offers){
-        int n= offers.size();
-        int l = idx, r = n-1, ans = -1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            int start = offers[mid][0];
-            if(start>lastIdx){
-                r = mid - 1;
-                ans = mid;
-            }
-            else{
-                l = mid + 1;
-            }
-        }
-        return ans;
+public:
+     vector<int>temp,dp;
+    int fn(int ind,vector<vector<int>>&offers)
+    {
+        if(ind>=offers.size())return 0;     
+        if(dp[ind]!=-1)return dp[ind];    
+        int Non_take=fn(ind+1,offers);  
+        int target=lower_bound(temp.begin(),temp.end(),offers[ind][1]+1)-temp.begin();
+        int take=offers[ind][2]+fn(target,offers);  
+        return dp[ind]=max(Non_take,take);
     }
-    int solve(int idx, int m, vector<int> &dp, vector<vector<int>> &offers){
-        int ans=0;
-        if(idx==m){
-            return 0;
-        }
-        if(dp[idx]!=-1)
-            return dp[idx];
-        // not pick
-        int maxProfit = solve(1 + idx, m, dp, offers);
-        // pick
-        // need to get the next index for which the houses can be sold
-        int nextIdx=findIdx(1+idx, offers[idx][1],offers);
-        // we will start searching from next index
-        if(nextIdx!=-1)
-            maxProfit = max(maxProfit, offers[idx][2] + solve(nextIdx, m, dp, offers));
-        else
-            maxProfit = max(maxProfit, offers[idx][2]);
-
-        return dp[idx] = maxProfit;
-    }
-
     int maximizeTheProfit(int n, vector<vector<int>>& offers) {
-        int m = offers.size();
-        sort(offers.begin(), offers.end());
-        vector<int> dp(m, -1);
-        return solve(0, m,dp,offers);
+        sort(offers.begin(),offers.end());
+        for(auto it:offers)
+            temp.push_back(it[0]);
+			
+        dp.resize(offers.size(),-1);
+        return fn(0,offers);
     }
 };
-
-
