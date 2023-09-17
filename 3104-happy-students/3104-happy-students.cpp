4b717a81e236a1eebe8ce065354fcf7b33337574
Dhruv Garg <77916132+dhruvg24@users.noreply.github.com>
2023-09-17 19:07:36 +0530

    Time: 91 ms (80%), Space: 72.2 MB (20%) - LeetHub

---
+++ 3104-happy-students/3104-happy-students.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countWays(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int cnt=0;
+        // if no student selecteed
+        if(nums[0]>0){
+            cnt++;
+        }
+        for(int i = 0;i<n-1;i++){
+            if(nums[i]<i+1 && 1+i<nums[i+1]){
+                cnt++;
+            }
+        }
+        if(nums[n-1]<n){
+            // if all are selected 
+            cnt++;
+        }
+        return cnt;
+    }
+};
